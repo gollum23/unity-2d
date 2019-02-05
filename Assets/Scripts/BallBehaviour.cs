02a329fe4456e0f3ba894588a5d74390c164480f
Diego Forero <gollum23@gmail.com>
2019-02-05 05:48:59 -0500

    :wrench: Collisions and canvas

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -6,6 +6,8 @@ public class BallBehaviour : MonoBehaviour
 {
     public Transform paddle;
     bool gameStarted = false;
+
+    public Rigidbody2D rbBall;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,7 @@ public class BallBehaviour : MonoBehaviour
             transform.position = new Vector3(paddle.position.x - posDiff, paddle.position.y, paddle.position.z);
             if(Input.GetMouseButtonDown(0))
             {
+                rbBall.velocity = new Vector2(8, 8);
                 gameStarted = true;
             }
         }
--- Assets/Scripts/MovePaddle.cs
+++ Assets/Scripts/MovePaddle.cs
@@ -13,7 +13,6 @@ public class MovePaddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(mousePos.y, -3.8f, 3.8f), transform.position.z);
